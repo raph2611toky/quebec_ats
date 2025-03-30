277bd79445e5a0f2bf4c57a658936a43e2496610
raph2611toky <raphaeltokinandrasana@gmail.com>
2025-03-30 21:49:53 +0300

    commit update

--- src/services/meet/googleMeet.services.js
+++ src/services/meet/googleMeet.services.js
@@ -1,10 +1,10 @@
 const { google } = require('googleapis');
 require("dotenv").config();
 
-CLIENT_ID=process.env.CLIENT_ID
-CLIENT_SECRET=process.env.CLIENT_SECRET
-REDIRECT_URI=process.env.REDIRECT_URI
-REFRESH_TOKEN=process.env.REFRESH_TOKEN
+CLIENT_ID=process.env.MEET_CLIENT_ID
+CLIENT_SECRET=process.env.MEET_CLIENT_SECRET
+REDIRECT_URI=process.env.MEET_REDIRECT_URI
+REFRESH_TOKEN=process.env.MEET_REFRESH_TOKEN
 
 const oauth2Client = new google.auth.OAuth2(
   CLIENT_ID,
