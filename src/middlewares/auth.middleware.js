79b811eeafca9c2216b5cd7f00df08bb1d1b1ddb
ANDRIANANTENAINA001Angelo <morningstar25angelo@gmail.com>
2025-03-31 17:58:54 +0300

    test

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -9,6 +9,7 @@ module.exports.IsAuthenticatedCandidat = async (req, res, next) => {
 
     try {
         const decoded = jwt.verifyToken(token);
+        console.log(decoded.role);
         
         if(decryptAES(decoded.role) !== "CANDIDAT"){
             res.status(401).json({ message: 'Token non autorisé.' });
